c0d4cd06da2ecc5207bc7f2a120c5fd59de9c8ca
Arshpreet Chabbewal <arsh.c321@gmail.com>
2024-07-15 07:20:44 -0400

    Fix typo in url

--- feedback-analyzer/src/App.js
+++ feedback-analyzer/src/App.js
@@ -6,7 +6,7 @@ function App() {
   const [individualSummaries, setIndividualSummaries] = useState([]);
   const [overallSummary, setOverallSummary] = useState('');
   const [downloadUrl, setDownloadUrl] = useState('');
-  const API_URL = 'http://cryptic-river-09912.herokuapp.com'
+  const API_URL = 'https://cryptic-river-09912.herokuapp.com'
 
   const handleFileChange = (event) => {
     setFile(event.target.files[0]);
